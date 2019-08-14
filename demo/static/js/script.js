494ac63d317da30f9a1e177b7643d992493e933a
Matheus Gomes <matheusdem0ura@protonmail.com>
2019-08-14 18:50:17 -0300

    Responsividade

--- demo/static/js/script.js
+++ demo/static/js/script.js
@@ -3,9 +3,9 @@ let nav = document.querySelector('nav');
 function mudar(){
     if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
         nav.style.backgroundColor = 'rgba(69,27,74)';  
-        nav.style.height = '80px'      
+        nav.style.height = '50px'      
     }else{
-        nav.style.backgroundColor = 'rgba(69,27,74,.0)';
+        nav.style.backgroundColor = 'rgba(69,27,74,.088)';
         nav.style.height = '120px' 
     }
 }
